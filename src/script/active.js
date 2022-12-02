e771907dd0057beb234b19baf19024dea93afbf4
afadillahazka <89429041+afadillahazka@users.noreply.github.com>
2022-12-02 19:48:23 +0700

    ncafa web v4.1

--- src/script/active.js
+++
@@ -1,2 +0,0 @@
-var activeNavlink = document.querySelectorAll('nav a[href^="/' + location.pathname.split("/")[1] + '"]');
-activeNavlink[0].classList.add('active');
--- src/script/menu.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.hamburgerButton').addEventListener('click', () => {
-    document.querySelector('.app-bar__navigation').classList.toggle('open')
-    document.querySelector('main').classList.toggle('blurred')
-  });
